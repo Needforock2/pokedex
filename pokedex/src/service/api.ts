9c3f490ff6f9ce936665f951d924a85d04be83a1
Needforock2 <needforock@gmail.com>
2024-10-30 16:53:11 -0300

    eslint fix

--- pokedex/src/service/api.ts
+++ pokedex/src/service/api.ts
@@ -78,35 +78,32 @@ export const getPaginatorData = async (): Promise<APIResponse> => {
   }
 };
 
-const pokemonTypes = {
-  "normal": 1,
-  "fighting": 2,
-  "flying": 3,
-  "poison": 4,
-  "ground": 5,
-  "rock": 6,
-  "bug": 7,
-  "ghost": 8,
-  "steel": 9,
-  "fire": 10,
-  "water": 11,
-  "grass": 12,
-  "electric": 13,
-  "psychic": 14,
-  "ice": 15,
-  "dragon": 16,
-  "dark": 17,
-  "fairy": 18,
-  "stellar": 19
-};
+const pokemonTypeList = [
+  "normal", "fighting", "flying", "poison", "ground",
+  "rock", "bug", "ghost", "steel", "fire",
+  "water", "grass", "electric", "psychic", "ice",
+  "dragon", "dark", "fairy", "stellar"
+];
 
-type PokemonTypeName = keyof typeof pokemonTypes;
+
 
 export const getPokemonType = async (
-  typeName: PokemonTypeName
+  typeName: string
 ): Promise<APIResponse> => {
+
+  const typeId = pokemonTypeList.indexOf(typeName) + 1;
+
+  if (typeId <= 0) {
+    return {
+      status: 400,
+      message: "Invalid type name",
+      data: [],
+    };
+  }
   try {
-    const url = `https://pokeapi.co/api/v2/type/${Number(pokemonTypes[typeName])}`;
+    
+
+    const url = `https://pokeapi.co/api/v2/type/${typeId}`;
     const response = await axios(url);
     const arrPokemons: PokemonElement[] = response.data.pokemon;
 

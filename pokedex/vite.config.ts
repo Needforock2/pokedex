aedc6bbfe38d63c6e83a7c94b70bb3b74bcaa88c
Needforock2 <needforock@gmail.com>
2024-11-07 10:55:35 -0300

    styles

--- pokedex/vite.config.ts
+++ pokedex/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom', // Aquí especificas jsdom como el entorno de prueba
+    environment: 'jsdom',
   },
 
 })
